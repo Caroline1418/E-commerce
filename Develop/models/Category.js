1b376ca930bfb7ede8c1a85317f2c1e5cc4557f0
Caroline Phillips <Caroline@Carolines-MacBook-Air.local>
2021-04-20 17:00:48 -0600

    first-commit

---
+++ Develop/models/Category.js
@@ -0,0 +1,25 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.Integer,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
